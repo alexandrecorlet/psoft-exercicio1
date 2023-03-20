7d67e22f09507cdbfe7b0eeaec471bad366c369b
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2023-03-20 11:06:46 -0300

    Cria ProdutoService

---
+++ src/service/ProdutoService.java
@@ -0,0 +1,29 @@
+package service;
+
+import java.util.Collection;
+
+import data.Produto;
+import repository.ProdutoRepository;
+
+public class ProdutoService {
+
+    private final ProdutoRepository produtoRepository;
+
+    public ProdutoService() {
+        this.produtoRepository = new ProdutoRepository();
+    }
+
+    public String criarProduto(String nome, String fabricante, double preco) {
+        Produto p = new Produto(nome, fabricante, preco);
+        return produtoRepository.adicionaProduto(p);
+    }
+
+    public Produto consultarProduto(String id) {
+        return produtoRepository.consultarProduto(id);
+    }
+
+    public Collection<Produto> listarProdutos() {
+        return produtoRepository.listarProdutos();
+    }
+
+}
